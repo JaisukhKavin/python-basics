919fee25098e745188a2b838497707a07694bc5e
JaisukhKavin <jaisukhkavins@gmail.com>
2024-07-21 19:40:09 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,10 @@
+# project 1
+"""colour=input('enter traffic light colour ')
+if (colour=='red'):
+    print('stop')
+elif (colour=='yellow'):
+    print('got ready')
+elif (colour=='green'):
+    print('go')
+else:
+    print('wrong colour')"""
